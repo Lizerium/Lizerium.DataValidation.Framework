86091ecdd9c546bf16b102aa74ef97cb69bb92a0
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-05-02 19:16:49 +0300

    Auto-update headers via pre_commit.ps1 - 2026-05-02

--- LizeriumTests/Components/Collections/ResultSearchCollection.cs
+++ LizeriumTests/Components/Collections/ResultSearchCollection.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 01 мая 2026 06:52:27
- * Version: 1.0.22
+ * Last Updated: 02 мая 2026 19:16:47
+ * Version: 1.0.23
  */
 
 using System;
