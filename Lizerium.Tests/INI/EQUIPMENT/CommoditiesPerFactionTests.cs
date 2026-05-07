c5fca97386c3e4e25f57b3d2ca894ea9f50b7596
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-05-07 15:27:15 +0300

    Auto-update headers via pre_commit.ps1 - 2026-05-07

--- Lizerium.Tests/INI/EQUIPMENT/CommoditiesPerFactionTests.cs
+++ Lizerium.Tests/INI/EQUIPMENT/CommoditiesPerFactionTests.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 06 мая 2026 10:29:41
- * Version: 1.0.27
+ * Last Updated: 07 мая 2026 15:27:13
+ * Version: 1.0.28
  */
 
 using System;
