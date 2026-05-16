5c255a2b0a22b829e1d14476cec65cde4ca5be89
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-05-16 10:47:12 +0300

    Auto-update headers via pre_commit.ps1 - 2026-05-16

--- LizeriumTests/Components/SettingsTestsData.cs
+++ LizeriumTests/Components/SettingsTestsData.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 15 мая 2026 07:50:00
- * Version: 1.0.36
+ * Last Updated: 16 мая 2026 10:47:11
+ * Version: 1.0.37
  */
 
 using Newtonsoft.Json;
