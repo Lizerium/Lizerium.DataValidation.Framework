a62b1f3472d31a001c36d97caaf99b03e26a34c2
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-05-29 13:54:52 +0300

    Auto-update headers via pre_commit.ps1 - 2026-05-29

--- LizeriumTests/Logic/CRC/CrcTool.cs
+++ LizeriumTests/Logic/CRC/CrcTool.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 28 мая 2026 17:30:35
- * Version: 1.0.49
+ * Last Updated: 29 мая 2026 13:54:50
+ * Version: 1.0.50
  */
 
 using System.Text;
