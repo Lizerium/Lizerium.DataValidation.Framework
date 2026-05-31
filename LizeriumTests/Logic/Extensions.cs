c90ed53fc851788f6f22751125e9f76d5c745c3a
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-05-31 14:55:15 +0300

    Auto-update headers via pre_commit.ps1 - 2026-05-31

--- LizeriumTests/Logic/Extensions.cs
+++ LizeriumTests/Logic/Extensions.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 30 мая 2026 15:43:07
- * Version: 1.0.51
+ * Last Updated: 31 мая 2026 14:55:14
+ * Version: 1.0.52
  */
 
 public static class Extensions
