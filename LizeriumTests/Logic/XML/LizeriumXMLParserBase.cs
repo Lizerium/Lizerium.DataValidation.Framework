1929b66cf1a045f7b52ad54a9bf58d069f3b0f90
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-03 15:16:38 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-03

--- LizeriumTests/Logic/XML/LizeriumXMLParserBase.cs
+++ LizeriumTests/Logic/XML/LizeriumXMLParserBase.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 02 июня 2026 06:52:26
- * Version: 1.0.54
+ * Last Updated: 03 июня 2026 15:16:36
+ * Version: 1.0.55
  */
 
 namespace LizeriumTests.Logic.XML
