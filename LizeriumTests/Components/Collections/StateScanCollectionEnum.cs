fff0bb7ac355a189eaa40c2469b6d6512103cc1e
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-05 06:52:24 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-05

--- LizeriumTests/Components/Collections/StateScanCollectionEnum.cs
+++ LizeriumTests/Components/Collections/StateScanCollectionEnum.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 04 июня 2026 06:52:25
- * Version: 1.0.56
+ * Last Updated: 05 июня 2026 06:52:22
+ * Version: 1.0.57
  */
 
 public enum StateScanCollectionEnum
