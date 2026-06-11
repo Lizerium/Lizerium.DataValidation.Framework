9d4b10a54cfd5a4ae840c6134bf296f905ebafc9
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-11 13:26:36 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-11

--- LizeriumTests/Logic/XML/AnimationXMLParser.cs
+++ LizeriumTests/Logic/XML/AnimationXMLParser.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 10 июня 2026 06:52:25
- * Version: 1.0.62
+ * Last Updated: 11 июня 2026 13:26:34
+ * Version: 1.0.63
  */
 
 using System.Linq;
