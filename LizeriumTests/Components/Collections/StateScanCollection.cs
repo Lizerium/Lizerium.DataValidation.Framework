e2f6e3a41901937582b761fb435cf2858d8205fc
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-27 13:13:56 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-27

--- LizeriumTests/Components/Collections/StateScanCollection.cs
+++ LizeriumTests/Components/Collections/StateScanCollection.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 26 июня 2026 06:52:22
- * Version: 1.0.78
+ * Last Updated: 27 июня 2026 13:13:54
+ * Version: 1.0.79
  */
 
 using LizeriumTests.Logic.Collections;
